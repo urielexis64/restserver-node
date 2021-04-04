cbd3294c3d8bb2b9294579ff7cf166342ee5bf93
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-03 17:46:59 -0700

    basic mongodb config

---
+++ db/config.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+	try {
+		mongoose.connect(process.env.MONGODB_ATLAS, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+
+		console.log("DB ONLINE");
+	} catch (error) {
+		console.log(error);
+		throw new Error("DB Connection error");
+	}
+};
+
+module.exports = {
+	dbConnection,
+};
