a525082159388a292412cd15e3f9ecf4c507c9fc
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-06 23:26:23 -0600

    authPath added

--- models/server.js
+++ models/server.js
@@ -6,7 +6,9 @@ class Server {
 	constructor() {
 		this.app = express();
 		this.port = process.env.PORT;
+
 		this.usersPath = "/api/users";
+		this.authPath = "/api/auth";
 
 		// Connect to DB
 		this.dbConnect();
@@ -34,6 +36,7 @@ class Server {
 	}
 
 	routes() {
+		this.app.use(this.authPath, require("../routes/auth.routes"));
 		this.app.use(this.usersPath, require("../routes/user.routes"));
 	}
 
