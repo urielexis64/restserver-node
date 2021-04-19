d9bcd44b232bf6f62470c7fd0d2eb274c354b298
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-19 00:01:34 -0600

    getCategories, getCategory and updateCategory

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,6 +1,30 @@
 const {request, response} = require("express");
 const {Category} = require("../models");
 
+// Get categories - paged - total - populate
+
+const getCategories = async (req = request, res = response) => {
+	const {limit = 2, offset = 0} = req.body;
+	const query = {status: true};
+
+	const [total, categories] = await Promise.all([
+		Category.count(query),
+		Category.find(query).skip(offset).limit(limit).populate("user", "name"),
+	]);
+
+	return res.json({total, categories});
+};
+
+// Get category - populate{}
+
+const getCategory = async (req = request, res = response) => {
+	const {id} = req.params;
+
+	const category = await Category.findById(id).populate("user", "name");
+
+	return res.json(category);
+};
+
 const createCategory = async (req = request, res = response) => {
 	const name = req.body.name.toUpperCase();
 
@@ -22,6 +46,29 @@ const createCategory = async (req = request, res = response) => {
 	res.status(201).json(category);
 };
 
+// Update category
+
+const updateCategory = async (req = request, res = response) => {
+	const {id} = req.params;
+	let {status, user, ...data} = req.body;
+
+	data.nombre = data.nombre.toUpperCase();
+	data.user = req.user._id;
+
+	if (!data) {
+		return res.status(400).json({message: "Missing body."});
+	}
+
+	const category = await Category.findByIdAndUpdate(id, data, {new: true});
+
+	return res.json(category);
+};
+
+// Delete category (status: false)
+
 module.exports = {
+	getCategories,
+	getCategory,
 	createCategory,
+	updateCategory,
 };
