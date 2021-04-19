3e4be0ba38491900d83de7f0b5b1c6e6d23b5585
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-19 00:02:19 -0600

    middlewares to getCategories, getCategory and updateCategory

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,33 +1,50 @@
 const {Router, request, response} = require("express");
 const {check} = require("express-validator");
 
+const {categoryExists} = require("../helpers/db_validators");
+
 const {validateJWT} = require("../middlewares/validate_jwt");
 const {validateFields} = require("../middlewares/validate_fields");
-const {createCategory} = require("../controllers/categories.controller");
+const {
+	createCategory,
+	getCategories,
+	getCategory,
+	updateCategory,
+} = require("../controllers/categories.controller");
 
 const router = Router();
 
 // Get all categories - public
-router.get("/", (req = request, res = response) => {
-	res.json({msg: "get"});
-});
+router.get("/", getCategories);
 
 // Get category by id - public
-router.get("/:id", (req = request, res = response) => {
-	res.json({msg: "get id"});
-});
+router.get(
+	"/:id",
+	[
+		check("id", "Id invalid.").not().isEmpty(),
+		check("id").custom(categoryExists),
+		validateFields,
+	],
+	getCategory
+);
 
 // Create category by id - private - any role
 router.post(
 	"/",
-	[validateJWT, check("name", "Cateogry name is required.").not().isEmpty(), validateFields],
+	[validateJWT, check("name", "Category name is required.").not().isEmpty(), validateFields],
 	createCategory
 );
 
-// Update category by id - private - any role
-router.put("/:id", (req = request, res = response) => {
-	res.json({msg: "put"});
-});
+// Update category by name - private - any role
+router.put(
+	"/:id",
+	[
+		check("id", "ID is required").not().isEmpty(),
+		check("id").custom(categoryExists),
+		validateFields,
+	],
+	updateCategory
+);
 
 // Delete category by id - private - admin role
 router.delete("/:id", (req = request, res = response) => {
