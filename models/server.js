a43d02bf82bb5e9e8975a8daec3b05acc3de9fbf
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-24 22:14:21 -0600

    product path

--- models/server.js
+++ models/server.js
@@ -11,6 +11,7 @@ class Server {
 			auth: "/api/auth",
 			users: "/api/users",
 			categories: "/api/categories",
+			products: "/api/products",
 		};
 
 		// Connect to DB
@@ -42,6 +43,7 @@ class Server {
 		this.app.use(this.paths.auth, require("../routes/auth.routes"));
 		this.app.use(this.paths.users, require("../routes/user.routes"));
 		this.app.use(this.paths.categories, require("../routes/categories.routes"));
+		this.app.use(this.paths.products, require("../routes/products.routes"));
 	}
 
 	listen() {
