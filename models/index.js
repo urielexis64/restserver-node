687e20688512f94992d0bc74d3c0ecd29ed9c75c
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-24 22:14:52 -0600

    product model imported

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const Category = require("./category");
 const Role = require("./role");
 const User = require("./user");
 const Server = require("./server");
+const Product = require("./product");
 
 module.exports = {
 	Category,
 	Role,
 	User,
 	Server,
+	Product,
 };
