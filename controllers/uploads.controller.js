da227c35f42b5fbfe2f24af526538cac2ca9b03a
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-27 12:21:49 -0600

    update image

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,12 +1,9 @@
 const {request, response} = require("express");
 const {uploadFile} = require("../helpers");
 
-const loadFile = async (req = request, res = response) => {
-	if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-		res.status(400).json({message: "No files were uploaded."});
-		return;
-	}
+const {User, Product} = require("../models");
 
+const loadFile = async (req = request, res = response) => {
 	try {
 		const filename = await uploadFile(req.files, ["txt", "md"], "text files");
 
@@ -16,6 +13,41 @@ const loadFile = async (req = request, res = response) => {
 	}
 };
 
+const updateImage = async (req = request, res = response) => {
+	const {collection, id} = req.params;
+
+	let model;
+
+	switch (collection) {
+		case "users":
+			model = await User.findById(id);
+			if (!model) {
+				return res.status(400).json({msg: `There is no user with the id ${id}`});
+			}
+			break;
+		case "products":
+			model = await Product.findById(id);
+			if (!model) {
+				return res.status(400).json({msg: `There is no product with the id ${id}`});
+			}
+			break;
+		default:
+			res.status(500).json({msg: "I forgot to validate this."});
+	}
+
+	try {
+		const filename = await uploadFile(req.files, undefined, collection);
+		model.img = filename;
+
+		await model.save();
+
+		res.json(model);
+	} catch (error) {
+		res.status(400).json({error});
+	}
+};
+
 module.exports = {
 	loadFile,
+	updateImage,
 };
