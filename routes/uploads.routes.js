daad6b4a3be15b6f63376358d7b557f022ea0492
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-27 15:58:01 -0600

    get user/product image route

--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const {check} = require("express-validator");
-const {loadFile, updateImage} = require("../controllers/uploads.controller");
+const {loadFile, updateImage, showImage} = require("../controllers/uploads.controller");
 const {validCollections} = require("../helpers");
 
 const {validateFields, validateFile} = require("../middlewares");
@@ -8,6 +8,7 @@ const {validateFields, validateFile} = require("../middlewares");
 const router = Router();
 
 router.post("/", validateFile, loadFile);
+
 router.put(
 	"/:collection/:id",
 	[
@@ -17,6 +18,16 @@ router.put(
 		validateFields,
 	],
 	updateImage
+);
+
+router.get(
+	"/:collection/:id",
+	[
+		check("id", "Invalid id").isMongoId(),
+		check("collection").custom((c) => validCollections(c, ["users", "products"])),
+		validateFields,
+	],
+	showImage
 );
 
 module.exports = router;
