356f228fb3fafca631c48e4d221574dd6fd2172c
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-27 15:58:13 -0600

    show image

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -56,7 +56,50 @@ const updateImage = async (req = request, res = response) => {
 	}
 };
 
+const showImage = async (req = request, res = response) => {
+	const {collection, id} = req.params;
+
+	let model;
+
+	switch (collection) {
+		case "users":
+			model = await User.findById(id);
+			if (!model) {
+				return res.status(400).json({msg: `There is no user with the id ${id}`});
+			}
+			break;
+		case "products":
+			model = await Product.findById(id);
+			if (!model) {
+				return res.status(400).json({msg: `There is no product with the id ${id}`});
+			}
+			break;
+		default:
+			res.status(500).json({msg: "I forgot to validate this."});
+	}
+
+	// Clear previous images
+	try {
+		let imagePath;
+
+		if (model.img) {
+			imagePath = path.join(__dirname, "../uploads/", collection, model.img);
+			if (!fs.existsSync(imagePath)) {
+				imagePath = path.join(__dirname, "../assets/placeholder.jpg");
+			}
+			return res.sendFile(imagePath);
+		}
+
+		imagePath = path.join(__dirname, "../assets/placeholder.jpg");
+
+		res.sendFile(imagePath);
+	} catch (error) {
+		res.status(400).json({error});
+	}
+};
+
 module.exports = {
 	loadFile,
 	updateImage,
+	showImage,
 };
