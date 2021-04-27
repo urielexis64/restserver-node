dc827856b649802f347b9cfea056500ec17b2af4
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-27 12:25:05 -0600

    update image endpoint

--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,11 +1,22 @@
 const {Router} = require("express");
 const {check} = require("express-validator");
-const {loadFile} = require("../controllers/uploads.controller");
+const {loadFile, updateImage} = require("../controllers/uploads.controller");
+const {validCollections} = require("../helpers");
 
-const {validateFields} = require("../middlewares/validate_fields");
+const {validateFields, validateFile} = require("../middlewares");
 
 const router = Router();
 
-router.post("/", loadFile);
+router.post("/", validateFile, loadFile);
+router.put(
+	"/:collection/:id",
+	[
+		validateFile,
+		check("id", "Invalid id").isMongoId(),
+		check("collection").custom((c) => validCollections(c, ["users", "products"])),
+		validateFields,
+	],
+	updateImage
+);
 
 module.exports = router;
