4ee7dc68fedb9a6258a588f756fb4bf3ccfdc14a
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-26 21:05:11 -0600

    load file

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,36 +1,17 @@
-const path = require("path");
-const {v4: uuidv4} = require("uuid");
 const {request, response} = require("express");
+const {uploadFile} = require("../helpers");
 
-const uploadFile = async (req = request, res = response) => {
+const loadFile = async (req = request, res = response) => {
 	if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
 		res.status(400).json({message: "No files were uploaded."});
 		return;
 	}
 
-	const {file} = req.files;
-	const cutName = file.name.split(".");
-	const extension = cutName[cutName.length - 1];
+	const filename = await uploadFile(req.files);
 
-	// Validating extension
-	const validExtensions = ["png", "jpg", "jpeg", "gif"];
-
-	if (!validExtensions.includes(extension)) {
-		res.status(400).json({msg: `Invalid file extension. ${extension}. (${validExtensions})`});
-	}
-
-	const tempName = uuidv4() + "." + extension;
-	const uploadPath = path.join(__dirname, "../uploads/", tempName);
-
-	file.mv(uploadPath, (err) => {
-		if (err) {
-			return res.status(500).json({err});
-		}
-
-		res.json({message: "File uploaded to " + uploadPath});
-	});
+	res.json({filename});
 };
 
 module.exports = {
-	uploadFile,
+	loadFile,
 };
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,11 +1,11 @@
 const {Router} = require("express");
 const {check} = require("express-validator");
-const {uploadFile} = require("../controllers/uploads.controller");
+const {loadFile} = require("../controllers/uploads.controller");
 
 const {validateFields} = require("../middlewares/validate_fields");
 
 const router = Router();
 
-router.post("/", uploadFile);
+router.post("/", loadFile);
 
 module.exports = router;
