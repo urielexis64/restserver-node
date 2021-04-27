d7fc1c6cab9e2d503406a75d719a82e18f20eba2
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-27 12:21:40 -0600

    validate file middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateFields = require("../middlewares/validate_fields");
 const validateJWT = require("../middlewares/validate_jwt");
 const validateRoles = require("../middlewares/validate_role");
+const validateFile = require("../middlewares/validate_file");
 
 module.exports = {
 	...validateFields,
 	...validateJWT,
 	...validateRoles,
+	...validateFile,
 };
